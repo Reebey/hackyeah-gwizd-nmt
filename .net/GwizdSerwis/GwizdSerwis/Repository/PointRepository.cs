ad69005f44b9577e3f93c363e0c2c99c9cfd2f89
mateusz <mateusz.w.air@gmail.com>
2023-10-01 10:55:32 +0200

    add refactor to create point

--- .net/GwizdSerwis/GwizdSerwis/Repository/PointRepository.cs
+++ .net/GwizdSerwis/GwizdSerwis/Repository/PointRepository.cs
@@ -4,6 +4,7 @@ using GwizdSerwis.Models.Incoming;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections;
+using System.Linq;
 
 namespace GwizdSerwis.Repository
 {
@@ -26,7 +27,17 @@ namespace GwizdSerwis.Repository
         {
             int userIdInt = Convert.ToInt32(userId);
             Point newPoint = new Point() { AuthorId = userIdInt, Latitude = point.Localization.Latitude, Longitude = point.Localization.Longitude, AnimalId = point.AnimalId.Value };
-            await _dbContext.Points.AddAsync(newPoint);
+            var firstNearPoint = _dbContext.Points.FirstOrDefault(p => CalculateDistance(newPoint, p) <= 1);
+
+            if (firstNearPoint != null)
+            {
+                _dbContext.Images.Add(new Image { PointId = firstNearPoint.Id });
+            }
+            else
+            {
+                await _dbContext.Points.AddAsync(newPoint);
+            }
+
             _dbContext.SaveChanges();
             return newPoint;
         }
@@ -39,6 +50,13 @@ namespace GwizdSerwis.Repository
         public async Task<IEnumerable<Point>> GetNewestPins(int limit)
         {
             return await _dbContext.Points.Include(p => p.Author).Include(p => p.Animal).Include(p => p.Images).OrderByDescending(p => p.Added).Take(limit).ToListAsync();
+        }
+
+        private double CalculateDistance(Point p1, Point p2)
+        {
+            double deltaX = p1.Longitude - p2.Longitude;
+            double deltaY = p1.Latitude - p2.Latitude;
+            return Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
         }
     }
 }
